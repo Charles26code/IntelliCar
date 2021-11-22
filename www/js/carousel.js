3e929907b43d3790734ac070924ae06a4009029b
Charles26code <64135991+Charles26code@users.noreply.github.com>
2021-11-22 10:52:52 +0100

    Fix carousel

--- www/js/carousel.js
+++ www/js/carousel.js
@@ -1,3 +1,27 @@
-$('.carousel').carousel({
-  interval: 2000
-})
+var num = 1;
+          function slide(side){
+              if(side =='right'){
+                num ++;
+                if(num >3){
+                  num=1;
+                  document.getElementById('slide-3').classList.remove('active');
+                  document.getElementById('slide-'+num).classList.add('active');
+                }else{
+                  document.getElementById('slide-'+(num-1)).classList.remove('active');
+                  document.getElementById('slide-'+num).classList.add('active');
+                }  
+                
+              }else if(side =='left'){
+                num--;
+                if(num <1){
+                  num=3;
+                  document.getElementById('slide-1').classList.remove('active');
+                  document.getElementById('slide-'+num).classList.add('active');
+                }else{
+                  document.getElementById('slide-'+(num+1)).classList.remove('active');
+                  document.getElementById('slide-'+num).classList.add('active');
+                }  
+              }
+                                        
+
+}
